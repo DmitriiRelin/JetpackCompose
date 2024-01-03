a80f6bc9b49ff977503cbb5821afbfc43c499c1f
d.relin <d.relin@start.ru>
2024-01-03 12:57:33 +0300

    Add new dependencies

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -4,21 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+import com.example.jetpackcompose.ui.theme.FirstComposeProjectTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTheme {
+            FirstComposeProjectTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
                     Greeting("Android")
                 }
             }
@@ -27,17 +30,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    JetpackComposeTheme {
+fun DefaultPreview() {
+    FirstComposeProjectTheme {
         Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/jetpackcompose/Sample.java
@@ -0,0 +1,18 @@
+package com.example.jetpackcompose;
+
+public class Sample {
+
+    public static void main () {
+        int number1 = 10;
+        int number2 = 15;
+
+        while (number1 < number2) {
+            System.out.println("Привет");
+            number2--;
+
+        }
+
+    }
+
+
+}
