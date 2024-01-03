b49ef3c8f9eea6d0250709ff2ce78a6f941de7fc
d.relin <d.relin@start.ru>
2024-01-03 14:17:04 +0300

    add Row, Box, Modifier

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,31 +3,13 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackcompose.composeElements.TimesTable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UserInfo(name = "John", age = 25)
-        }
-    }
-}
-
-@Preview
-@Composable
-fun UserInfoPreview() {
-    UserInfo(name = "John", age = 25)
-}
-
-@Composable
-fun UserInfo(name: String, age: Int) {
-    Column() {
-        repeat(10) {
-            Text(text = "Hello $name! You are $age years old")
+            TimesTable()
         }
     }
 }
