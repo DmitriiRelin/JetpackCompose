b776d41b6c7c3552d926cce48cbdce05809304ce
d.relin <d.relin@start.ru>
2024-01-04 10:33:02 +0300

    Add PostCard

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,28 +3,14 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.jetpackcompose.composeElements.InstagramProfileCard
-import com.example.jetpackcompose.composeElements.TimesTable
+import com.example.jetpackcompose.composeElements.PostCard
 import com.example.jetpackcompose.ui.theme.ComposeProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,16 +22,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier
                         .fillMaxSize()
                         .background(MaterialTheme.colors.background)
+                        .padding(8.dp)
+
                 ) {
-                    InstagramProfileCard()
+                    PostCard()
                 }
             }
         }
     }
-}
-
-@Preview
-@Composable
-fun text() {
-    Image(painterResource(id = R.drawable.ic_launcher_background), contentDescription = "")
 }
